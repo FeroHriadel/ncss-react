e00a7c0e3787d2d30ba6fc7eb175d9561a0d2231
Fero Hriadel <ferdinand.hriadel@gmail.com>
2025-10-26 16:56:51 +0100

    closable pill

--- src/components/pills/Pill.tsx
+++ src/components/pills/Pill.tsx
@@ -1,3 +1,4 @@
+import { FaTimes } from 'react-icons/fa';
 
 
 
@@ -6,13 +7,19 @@ export interface PillProps {
   id?: string;
   className?: string;
   style?: React.CSSProperties;
+  onClick?: React.MouseEventHandler<HTMLSpanElement>;
+  onClose?: (params?: string | number | undefined | null | object | React.MouseEvent<HTMLSpanElement>) => void;
 }
 
 
 
-export default function Pill({ children, className, style }: PillProps) {
+export default function Pill({ children, className, style, onClose, onClick = () => {} }: PillProps) {
   return (
-    <span className={`text-sm font-semibold text-gray-600 bg-gray-100 px-3 py-1 rounded ${className}`} style={style}>
+    <span 
+      className={`text-sm font-semibold text-gray-600 bg-gray-100 px-3 py-1 rounded ${className}`}    style={style}
+      onClick={onClick}
+    >
+      {onClose && <FaTimes className='mr-2' onClick={onClose} />}
       {children}
     </span>
   )
